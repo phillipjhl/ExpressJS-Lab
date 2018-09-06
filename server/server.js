a908bbb808557383a1b33bddf0411f9082a48301
Phillip <36901887+phillipjhl@users.noreply.github.com>
2018-09-06 16:31:11 -0500

    got post from form, sent response back

--- server/server.js
+++ server/server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const bP = require('body-parser');
 const app = express();
 
-//middleware logger
-app.use((req, res, next) => {
-    console.log(req.originalUrl);
-    next();
+app.use(bP.urlencoded({extended: false}));
+
+app.post('/contact-form', (req, res) => {
+    console.log(req.body.email);
+    console.log(req.body.name);
+    res.send('Thank You!');
 });
 
+//middleware url logger
+// app.use((req, res, next) => {
+    //     console.log(req.originalUrl);
+    //     next();
+    // });
+    
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.listen(3000);
